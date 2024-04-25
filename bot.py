7c99c9f28b61a407e58d885e08f6d41549ee69cb
Liya <liya.isaeva@gmail.com>
2024-04-25 18:00:04 +0100

    Final state

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from aiogram import Bot, Dispatcher, executor
 from aiogram.dispatcher.filters import Text
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from pymysql import connect
 from requests import get
+from aiogram.types import ReplyKeyboardRemove
 
-from keyboards import generate_cities_list
+from keyboards import generate_cities_list, generate_save_city_button
 
 API_KEY = "98943b3a7c43ba4865aaa7f64ff282e1"
 URL = "https://api.openweathermap.org/data/2.5/weather"
@@ -21,30 +22,64 @@ connection = connect(
 )
 cursor = connection.cursor()
 
+def fetch_cities(user):
+    cursor.execute("""SELECT * FROM cities WHERE user_id = %s""", (user[0],))
+    cities = cursor.fetchall()
+    return cities
+
+
 @dp.message_handler(commands='start')
 async def say_hello(message: Message):
     telegram_id = message.from_user.id
     full_name = message.from_user.full_name
 
-    cursor.execute("""SELECT * FROM users WHERE user_id = %s""", (telegram_id))
+    
+    cursor.execute("""SELECT * FROM users WHERE user_id = %s""", (telegram_id,))
     user = cursor.fetchone()
 
+    
+    cities = fetch_cities(user)
+
+    # TODO: Cities list is not updated automatically on every start, needs to be fixed
+    
+
     if not user:
-        cursor.execute("""INSERT INTO users(full_name, user_id) VALUES (%s, %s)""", (full_name, telegram_id))
+        cursor.execute(
+            """INSERT INTO users(full_name, user_id) VALUES (%s, %s)""", (full_name, telegram_id))
         connection.commit()
-        await bot.send_message(chat_id=telegram_id, text="We are glad to seee you there !",  reply_markup=generate_cities_list())
+        await bot.send_message(chat_id=telegram_id, text="We are glad to seee you there !", reply_markup=generate_cities_list())
 
     else:
-        await bot.send_message(chat_id=telegram_id, text="Welcome back !")
+        await bot.send_message(chat_id=telegram_id, text="Welcome back !",  reply_markup=generate_cities_list(cities))
+
+
+@dp.message_handler(commands='clear')
+async def clear_cities_list(message: Message):
+    telegram_id = message.from_user.id
     
-    await bot.send_message(chat_id=telegram_id, text="Type city name to find out weather informtion here.",  reply_markup=generate_cities_list())
+    # Identify user id by telegram id from users table
+    cursor.execute("""SELECT * FROM users WHERE user_id = %s""", (telegram_id,))
+    user = cursor.fetchone()
+    
+    # Clear cities list of this user from cities table
+    cursor.execute("""DELETE FROM cities WHERE user_id = %s""", (user[0],))
+    connection.commit()
+    
+    # Update cities list for this user
+    cities = fetch_cities(user=user)
+
+    # Inform user about this operation
+    await bot.send_message(chat_id=telegram_id,
+                           text="Your cities list has bees successfully cleaned",
+                           reply_markup=ReplyKeyboardRemove())
+
 
 
 @dp.message_handler()
 async def answer_weather_data(message: Message):
     chat_id = message.from_user.id
 
-    
+
     try:
         PARAMS = {
             'q': message.text,
@@ -59,15 +94,14 @@ async def answer_weather_data(message: Message):
         temp_max = data["main"]["temp_max"]
         humidity = data["main"]["humidity"]
         pressure = data["main"]["pressure"]
-        desc = data["weather"][0]["description"]
+        desc = data["weather"][0]["description"].capitalize()
         wind_speed = data["wind"]["speed"]
-        sunrise = datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%m")
-        sunset = datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%m")
-
-
+        # sunrise = datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%m")
+        # sunset = datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%m")
+        # timezone = data["timezone"]  # 18000 => 5 hours
 
         text = f"""🏙 Today in <b>{message.text}</b>
-        
+
 🌞Temperature: <b>{temp} C°</b>
 ☀️ Feels like: <b>{feels_like} C°</b>
 ❄️ Min. temperature: <b>{temp_min} C°</b>
@@ -78,10 +112,49 @@ async def answer_weather_data(message: Message):
 📃 Description: <b>{desc}</b>
 💨 Wind speed: <b>{wind_speed} km/h</b>
 """
+        # Sunrise: {sunrise}
+        # Sunset: {sunset}
+        # Timezone: {timezone // 3600} hour(s) 
+        await bot.send_message(chat_id=chat_id, text=text, parse_mode="HTML", reply_markup=generate_save_city_button(city_name=message.text.capitalize()))
 
-        await bot.send_message(chat_id=chat_id, text=text, parse_mode="HTML")
-  
     except:
-        await bot.send_message(chat_id=chat_id, text="Invalid city name.")
+        await bot.send_message(chat_id=chat_id, text="Invalid city name")
+
+
+@dp.callback_query_handler()
+async def save_city_to_cities_list(callback: CallbackQuery):
+
+    #
+    city_name = callback.data
+
+    
+    telegram_id = callback.from_user.id
+    
+    
+    cursor.execute("""SELECT id FROM users WHERE user_id = %s""", (telegram_id,))
+    user_id = cursor.fetchone()
+
+    
+    cursor.execute("""SELECT * FROM cities WHERE user_id = %s AND city_name = %s""", (user_id, city_name))
+    city = cursor.fetchone()
+
+    if not city:
+        
+        cursor.execute("""INSERT INTO cities (user_id, city_name) VALUES (%s, %s)""", 
+                       (user_id, city_name))
+        connection.commit()
+
+        
+        cities = fetch_cities(user=(user_id, ""))
+
+        
+        await bot.send_message(chat_id=telegram_id, 
+                           text=f"{city_name} was saved successfully", 
+                           reply_markup=generate_cities_list(cities))
+    
+    else:
+        
+        await bot.send_message(chat_id=telegram_id,
+                               text=f"You already have {city_name} in your cities list")
 
 executor.start_polling(dispatcher=dp)
--- keyboards.py
+++ keyboards.py
@@ -1,13 +1,34 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton # Inline buttons => attached to message
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def generate_cities_list():
+def generate_cities_list(cities = []):
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 
-    button1 = KeyboardButton(text="Southampton")
-    button2 = KeyboardButton(text="Winchester")
-    button3 = KeyboardButton(text="Tashkent")
 
-    keyboard.row(button1, button2)
-    keyboard.row(button3)
+    in_row = 2
+    start = 0
+    end = in_row
+    rows = len(cities) // in_row
+
+    if len(cities) % in_row != 0:
+        rows += 1
+    
+    for _ in range(rows):
+        buttons = []
+        for city_id, user_id, city_name in cities[start:end]:
+            buttons.append(
+                KeyboardButton(text=city_name)
+            )
+        keyboard.row(*buttons)
+        start = end
+        end += in_row
+
+    return keyboard
+
+
+def generate_save_city_button(city_name):
+    keyboard = InlineKeyboardMarkup()
+    button = InlineKeyboardButton(text=f"Save {city_name}", callback_data=city_name)
+    keyboard.add(button)
 
     return keyboard
